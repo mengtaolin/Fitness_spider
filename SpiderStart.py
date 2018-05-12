d563b74c2c9d4b1f0dceba6ca281d9968860f4d4
mengtaolin <1187352588@qq.com>
2018-05-13 00:29:36 +0800

    Initial commit

---
+++ SpiderStart.py
@@ -0,0 +1,12 @@
+class Spider():
+    def __init__(self):
+        pass;
+
+    def startSpider(self):
+        pass;
+
+
+
+if __name__ == "__main__":
+    spider = Spider();
+    spider.startSpider();
